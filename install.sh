3f2588be9fc2dcc336b16269657c7e1e70b7a403
0fariid0 <126415296+0fariid0@users.noreply.github.com>
2024-08-02 12:00:49 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Define script paths
-SCRIPT_URL="https://raw.githubusercontent.com/0fariid0/PsiphonLinux/main/psiphon_manager.sh"
-SCRIPT_PATH="/usr/local/bin/psiphon_manager.sh"
+# Define the path for the scripts
+TUNNEL_SCRIPT_PATH="/usr/local/bin/tunnel.sh"
+UPDATE_SCRIPT_PATH="/usr/local/bin/update_script.sh"
 
-# Download the Psiphon manager script
-echo "Downloading Psiphon manager script..."
-wget $SCRIPT_URL -O $SCRIPT_PATH
+# Download the tunnel script
+echo "Downloading the latest tunnel script..."
+wget https://raw.githubusercontent.com/0fariid0/tunel/main/tunnel.sh -O $TUNNEL_SCRIPT_PATH
 
-# Make the script executable
-echo "Setting executable permissions..."
-chmod +x $SCRIPT_PATH
+# Download the update script
+echo "Downloading the update script..."
+wget https://raw.githubusercontent.com/0fariid0/tunel/main/update_script.sh -O $UPDATE_SCRIPT_PATH
+
+# Make the scripts executable
+chmod +x $TUNNEL_SCRIPT_PATH
+chmod +x $UPDATE_SCRIPT_PATH
+
+# Execute the tunnel script
+echo "Executing the tunnel script..."
+sudo $TUNNEL_SCRIPT_PATH
 
 # Inform the user
-echo "Psiphon manager script installed successfully."
-
-# Run the Psiphon manager script in the background and log output
-echo "Running the Psiphon manager script..."
-nohup $SCRIPT_PATH > /var/log/psiphon_manager.log 2>&1 &
+echo "The tunnel script has been executed successfully."
