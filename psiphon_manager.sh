65d06c28516aa33788811371aad61b42e5ba730a
0fariid0 <126415296+0fariid0@users.noreply.github.com>
2024-08-02 11:56:20 +0330

    Update psiphon_manager.sh

--- psiphon_manager.sh
+++ psiphon_manager.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Function to install Psiphon
 install_psiphon() {
     echo "Downloading Psiphon installer..."
     wget https://raw.githubusercontent.com/0fariid0/PsiphonLinux/main/plinstaller2 -O plinstaller2
@@ -13,14 +14,24 @@ install_psiphon() {
     echo "Psiphon installed successfully."
 }
 
+# Function to remove Psiphon
 remove_psiphon() {
     echo "Removing Psiphon..."
+    sudo systemctl stop psiphon
+    sudo systemctl disable psiphon
     sudo apt-get remove --purge psiphon -y
     sudo rm /usr/local/bin/psiphon_manager.sh
 
     echo "Psiphon removed successfully."
 }
 
+# Function to exit the script
+exit_script() {
+    echo "Exiting..."
+    exit 0
+}
+
+# Main menu loop
 while true; do
     echo "Please choose an option:"
     echo "1. Install Psiphon"
@@ -31,7 +42,7 @@ while true; do
     case $choice in
         1) install_psiphon ;;
         2) remove_psiphon ;;
-        3) exit ;;
+        3) exit_script ;;
         *) echo "Invalid option. Please try again." ;;
     esac
 done
