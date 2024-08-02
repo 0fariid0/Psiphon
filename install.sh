006a91a4a58410cf6cc6ffae3b3b5b5a4fb8c045
0fariid0 <126415296+0fariid0@users.noreply.github.com>
2024-08-02 11:50:57 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# Define script paths
+SCRIPT_URL="https://raw.githubusercontent.com/0fariid0/PsiphonLinux/main/psiphon_manager.sh"
+SCRIPT_PATH="/usr/local/bin/psiphon_manager.sh"
+
 # Download the Psiphon manager script
 echo "Downloading Psiphon manager script..."
-wget https://raw.githubusercontent.com/0fariid0/PsiphonLinux/main/psiphon_manager.sh -O /usr/local/bin/psiphon_manager.sh
+wget $SCRIPT_URL -O $SCRIPT_PATH
 
 # Make the script executable
-chmod +x /usr/local/bin/psiphon_manager.sh
+echo "Setting executable permissions..."
+chmod +x $SCRIPT_PATH
 
 # Inform the user
 echo "Psiphon manager script installed successfully."
 echo "Running the Psiphon manager script..."
 
 # Run the script
-/usr/local/bin/psiphon_manager.sh
+$SCRIPT_PATH
